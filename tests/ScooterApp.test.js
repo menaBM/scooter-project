26ad414b52bfe76193aa5c5240e99b1ad9bf542d
menaBM <mena.bousfieldmilton@sky.uk>
2022-10-21 11:33:31 +0100

    created the tests for the scooter app class

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,13 +1,57 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
+const { TestWatcher } = require('jest/build/jest')
 
 // ScooterApp tests here
-
+describe("testing the user class interactions with the scooter app class", ()=>{
 // register user
-
+    beforeEach(()=>{
+        user = new User("alex19","superStrongPassword", 32);
+        app = new ScooterApp()
+    })
+    test("a user can be registered", ()=>{
+        app.register(user)
+        expect(app.registeredUsers.keys()).toContain(user.username)
+    })
+    test("a user cannot register if they are already registered", ()=>{
+        jest.spyOn(console.log)
+        app.register(user)
+        expect(console.log).toHaveBeenCalledWith("already registered")
+    })
+    test("a user must be over 17 to register", ()=>{
+        jest.spyOn(console.log)
+        app.register(new User("lawrence", "aProperlyHashedPassword", 14))
+        expect(console.log).toHaveBeenCalledWith("too young to register!")
+    })
 // log in
-
+    test("a user can log in", ()=>{
+        app.logIn(user.username, user.password)
+        expect(registeredUsers[user.username].loggedIn).toEqual(true)
+    })
+    test("a user with an incorrect username or password cannot log in",()=>{
+        expect(app.logIn(user.username, "abcdefg")).toThrow("Username or password is incorrect.")
+        expect(app.logIn("not_a_user", "abcdefghijk")).toThrow("Username or password is incorrect.")
+    })
+})
+describe("testing the scooter class interactions with the scooter app class",()=>{
+    beforeEach(()=>{
+        scooter = new Scooter("initalCity", "thomas")
+        app = new ScooterApp()
+    })
 // add scooter
-
+    test('adding a scooter', ()=>{
+        app.addScooter("city", scooter)
+        expect(scooter.station).toBe("city")
+        expect(app.stations["city"]).toContain(scooter)
+    })
 // remove scooter
+    test("removing a scooter", ()=>{
+        jest.spyOn(console.log)
+        app.removeScooter(scooter)
+        expect(console.log).toHaveBeenCalledWith("scooter removed succesfully")
+    })
+    test("removing a scooter that is not currently in the list",()=>{
+        expect(app.removeScooter(scooter)).toThrow("this scooter is not in the list")
+    })
+})
