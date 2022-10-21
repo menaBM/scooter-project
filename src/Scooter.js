2866f42ca36fd60df170cd786160c8a547e69fcb
menaBM <mena.bousfieldmilton@sky.uk>
2022-10-21 13:06:59 +0100

    making the scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,43 @@
 class Scooter{
-  // scooter code here
+  constructor(station, user){
+    this.station = station
+    this.user = user
+    this.serial = Math.floor(Math.random()*1000)+1
+    this.charge = Math.floor(Math.random()* 100) + 1
+    this.isBroken = false
+    this.docked = true
+  }
+  rent(){
+    if (this.isBroken === true){
+      throw new Error( "Scooter is broken, please send a repair request.")
+    }
+    else if (this.charge<=20 ) {
+        throw new Error( "Scooter low on battery, please charge.")
+      }
+    this.docked = false
+    console.log("Enjoy the ride!")
+  }
+  dock(station){
+    if (station === undefined){
+      throw new Error("Docking station required!")
+    }
+    this.station = station
+    this.docked = true
+    this.user = ""
+  }
+  async recharge(){
+    console.log('Charging scooter...');
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    this.charge = 100;
+    console.log('Fully charged.');   
+  }
+    async requestRepair(){
+      console.log("sending scooter for repairs")
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      console.log("scooter repaired")
+      this.isBroken = false
+    }
+
 }
 
 
