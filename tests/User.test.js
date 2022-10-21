e6cade50d654d08e259256f1d19c98a119273268
menaBM <mena.bousfieldmilton@sky.uk>
2022-10-21 10:42:45 +0100

    creating the tests and class for the user

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,9 +1,18 @@
 const User = require('../src/User')
 
 // User tests here
-
+describe("creating an instance of user", ()=> {
+    beforeEach(() => {user = new User("Jane","securePassword!", 21)})
 // test username
-
+    test("checking the username", ()=>{
+        expect(user.username).toBe("Jane")
+    }),
 // test password
-
+    test("checking the password property",()=>{
+        expect(user.password).toBe("securePassword!")
+    })
 // test age
+    test("checking the age property", ()=>{
+        expect(user.age).toBe(21)
+    })
+})
